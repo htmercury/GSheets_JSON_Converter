ab46f0483355932964badd124a7161e380dcbda3
Ka Wong <kawong2020@u.northwestern.edu>
2017-12-21 21:37:33 -0600

    Added new properties for OKrammus

--- main.py
+++ main.py
@@ -12,11 +12,10 @@ client = gspread.authorize(creds)
 sheet = client.open(os.environ['GSheet']).sheet1
 pp = pprint.PrettyPrinter()
 
-inputLabel = ["_id", "name", "flags", "quotes", "created_date", "__v"]
+inputLabel = ["_id", "name", "flags", "quotes", "image", "direction", "created_date", "__v"]
 DB = {}
 MAX_LENGTH = 138 # CURRENTLY 138 CHAMPIONS
 ENTRY_LENGTH = len(inputLabel)
-
 
 # CLEAR THE JSON FILE
 with open('output.json', 'w') as f:
@@ -25,16 +24,17 @@ print(f.closed)
 
 for i in range(2, MAX_LENGTH):
     data = sheet.row_values(i)[:ENTRY_LENGTH] # grab our data on a specific row
-
     # INPUT DICT
     DB["_id"] = data[0]
-    DB["flags"] = data[1]
     DB["name"] = data[2]
+    DB["flags"] = data[1]
+    DB["quotes"] = literal_eval(data[7])
+    DB["image"] = data[5]
+    DB["direction"] = data[6]
     DB["created_date"] = data[3]
     DB["__v"] = int(data[4])
-    DB["quotes"] = literal_eval(data[5])
     
     with open('output.json', 'a') as f:
         f.write(json.dumps(DB) + "\n")
     # DUMP TO JSON FILE
-    print(json.dumps(DB))
+    print(json.dumps(DB)) # DEBUGGING PURPOSES
